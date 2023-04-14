5d1c37f52085c6ebf866799f71017a106600c217
Matt Kazmar <mkazmar@aviatrix.com>
2023-04-14 11:14:03 -0500

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "gcp_instance" {
   description = "The GCP instance resource."
-  value = google_compute_instance.this
+  value       = google_compute_instance.this
+}
+
+output "name_ip" {
+  description = "Hash of name/IP"
+  value       = { (google_compute_instance.this.name) = [google_compute_instance.this.network_interface.0.access_config.0.nat_ip, google_compute_instance.this.network_interface.0.network_ip] }
 }
--- variables.tf
+++ variables.tf
@@ -1,42 +1,42 @@
 variable "region" {
   description = "GCP Region for this VM."
-  type = string
+  type        = string
 }
 
 variable "ssh_public_key" {
   description = "The ssh public key."
-  type = string
+  type        = string
 }
 
 variable "instance_name" {
   description = "Name of the instance."
-  type = string
+  type        = string
 }
 
 variable "machine_type" {
   description = "Size of instance."
-  type = string
-  default = "e2-small"
+  type        = string
+  default     = "e2-small"
 }
 
 variable "security_group_id" {
   description = "Security Group ID"
-  type = string
+  type        = string
 }
 
 variable "subnetwork_self_link" {
   description = "Subnet Self_link"
-  type = string
+  type        = string
 }
 
 variable "network_tags" {
   description = "Map of tags to apply to the instance."
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "zone_number" {
   description = "Zone number index as provided by the google_compute_zones data resource."
-  type = number
-  default = 0
+  type        = number
+  default     = 0
 }
